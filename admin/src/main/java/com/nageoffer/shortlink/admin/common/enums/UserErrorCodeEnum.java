ef3317fc287ef2fc74481b4630ed43c0162ec925
chen.ma <machen@apache.org>
2023-10-18 23:14:41 +0800

    feature: 开发用户自定义异常码

---
+++ admin/src/main/java/com/nageoffer/shortlink/admin/common/enums/UserErrorCodeEnum.java
@@ -0,0 +1,29 @@
+package com.nageoffer.shortlink.admin.common.enums;
+
+import com.nageoffer.shortlink.admin.common.convention.errorcode.IErrorCode;
+
+public enum UserErrorCodeEnum implements IErrorCode {
+
+    USER_NULL("B000200", "用户记录不存在"),
+
+    USER_EXIST("B000201", "用户记录已存在");
+
+    private final String code;
+
+    private final String message;
+
+    UserErrorCodeEnum(String code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    @Override
+    public String code() {
+        return code;
+    }
+
+    @Override
+    public String message() {
+        return message;
+    }
+}
