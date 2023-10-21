8fe6cd90360353a02fc2aa2df3fbb0c059ba22ad
chen.ma <machen@apache.org>
2023-10-21 18:51:03 +0800

    feature: 用户表通过ShardingSphere完成分表处理

---
+++ admin/src/test/java/com/nageoffer/shortlink/admin/test/UserTableShardingTest.java
@@ -0,0 +1,25 @@
+package com.nageoffer.shortlink.admin.test;
+
+public class UserTableShardingTest {
+
+    public static final String SQL = "CREATE TABLE `t_user_%d` (\n" +
+            "  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',\n" +
+            "  `username` varchar(256) DEFAULT NULL COMMENT '用户名',\n" +
+            "  `password` varchar(512) DEFAULT NULL COMMENT '密码',\n" +
+            "  `real_name` varchar(256) DEFAULT NULL COMMENT '真实姓名',\n" +
+            "  `phone` varchar(128) DEFAULT NULL COMMENT '手机号',\n" +
+            "  `mail` varchar(512) DEFAULT NULL COMMENT '邮箱',\n" +
+            "  `deletion_time` bigint(20) DEFAULT NULL COMMENT '注销时间戳',\n" +
+            "  `create_time` datetime DEFAULT NULL COMMENT '创建时间',\n" +
+            "  `update_time` datetime DEFAULT NULL COMMENT '修改时间',\n" +
+            "  `del_flag` tinyint(1) DEFAULT NULL COMMENT '删除标识 0：未删除 1：已删除',\n" +
+            "  PRIMARY KEY (`id`),\n" +
+            "  UNIQUE KEY `idx_unique_username` (`username`) USING BTREE\n" +
+            ") ENGINE=InnoDB AUTO_INCREMENT=1715030926162935810 DEFAULT CHARSET=utf8mb4;";
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 16; i++) {
+            System.out.printf((SQL) + "%n", i);
+        }
+    }
+}
