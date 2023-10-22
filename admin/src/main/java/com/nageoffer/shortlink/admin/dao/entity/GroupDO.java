45678911dbb59347266f862ef8d3a9422bdc364e
chen.ma <machen@apache.org>
2023-10-22 14:24:37 +0800

    feature: 开发短链接后管系统分组排序字段

--- admin/src/main/java/com/nageoffer/shortlink/admin/dao/entity/GroupDO.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/dao/entity/GroupDO.java
@@ -36,4 +36,9 @@ public class GroupDO extends BaseDO {
      * 创建分组用户名
      */
     private String username;
+
+    /**
+     * 分组排序
+     */
+    private Integer sortOrder;
 }
--- admin/src/main/java/com/nageoffer/shortlink/admin/service/impl/GroupServiceImpl.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/service/impl/GroupServiceImpl.java
@@ -25,6 +25,7 @@ public class GroupServiceImpl extends ServiceImpl<GroupMapper, GroupDO> implemen
         } while (!hasGid(gid));
         GroupDO groupDO = GroupDO.builder()
                 .gid(gid)
+                .sortOrder(0)
                 .name(groupName)
                 .build();
         baseMapper.insert(groupDO);
