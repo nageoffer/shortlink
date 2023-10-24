9d4841e78bf587b5ddda3fd5ca76b43c50e4d551
chen.ma <machen@apache.org>
2023-10-24 22:43:01 +0800

    optimize: 优化短链接分组查询返回实体

--- admin/src/main/java/com/nageoffer/shortlink/admin/dto/resp/ShortLinkGroupRespDTO.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/dto/resp/ShortLinkGroupRespDTO.java
@@ -19,11 +19,6 @@ public class ShortLinkGroupRespDTO {
     private String name;
 
     /**
-     * 创建分组用户名
-     */
-    private String username;
-
-    /**
      * 分组排序
      */
     private Integer sortOrder;
