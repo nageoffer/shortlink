a2b1c43a7d55b6265c0199422eac9684c7e859c4
chen.ma <machen@apache.org>
2023-10-26 08:41:46 +0800

    feature: 前端传递有效期时间序列化

--- admin/src/main/java/com/nageoffer/shortlink/admin/remote/dto/req/ShortLinkCreateReqDTO.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/remote/dto/req/ShortLinkCreateReqDTO.java
@@ -1,5 +1,6 @@
 package com.nageoffer.shortlink.admin.remote.dto.req;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.util.Date;
@@ -38,6 +39,7 @@ public class ShortLinkCreateReqDTO {
     /**
      * 有效期
      */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date validDate;
 
     /**
