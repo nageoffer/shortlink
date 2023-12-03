053dda7bb6d623f3cc2d57de27d34c5a80940b5c
chen.ma <machen@apache.org>
2023-12-03 17:12:45 +0800

    optimize: 优化短链接后管用户返回数据代码

--- admin/src/main/java/com/nageoffer/shortlink/admin/common/biz/user/UserTransmitFilter.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/common/biz/user/UserTransmitFilter.java
@@ -90,17 +90,10 @@ public class UserTransmitFilter implements Filter {
     }
 
     private void returnJson(HttpServletResponse response, String json) throws Exception {
-        PrintWriter writer = null;
         response.setCharacterEncoding("UTF-8");
         response.setContentType("text/html; charset=utf-8");
-        try {
-            writer = response.getWriter();
+        try (PrintWriter writer = response.getWriter()) {
             writer.print(json);
-
-        } catch (IOException e) {
-        } finally {
-            if (writer != null)
-                writer.close();
         }
     }
 }
