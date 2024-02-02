b4a0a0385530cf027f7d124ddac309eb1d515263
chen.ma <machen@apache.org>
2024-02-02 15:10:00 +0800

    docs: 更新数据库初始化数据文件

---
+++ resources/database/link-data.sql
@@ -0,0 +1,8 @@
+INSERT INTO `t_group_15` (`id`, `gid`, `name`, `username`, `sort_order`, `create_time`, `update_time`, `del_flag`)
+VALUES (1752265619253805057, 'tSUBMP', '默认分组', 'admin', 0, '2024-01-31 21:00:00', '2024-01-31 21:00:00', 0);
+
+
+INSERT INTO `t_user_15` (`id`, `username`, `password`, `real_name`, `phone`, `mail`, `deletion_time`, `create_time`,
+                         `update_time`, `del_flag`)
+VALUES (1752265616481370113, 'admin', 'admin123456', 'admin', 'yKZz0xLyjNb9LSCOCfJD4w==', '02/9oF/nWTBK0cM8UPtCOw==',
+        NULL, '2024-01-31 21:00:00', '2024-01-31 21:00:00', 0);
