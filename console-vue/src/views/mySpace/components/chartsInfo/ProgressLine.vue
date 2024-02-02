05b2ca767ef34f84ec002f37cb4671e558fec45e
chen.ma <machen@apache.org>
2024-02-02 13:27:33 +0800

    optimize: 优化前端展示效果

--- console-vue/src/views/mySpace/components/chartsInfo/ProgressLine.vue
+++ console-vue/src/views/mySpace/components/chartsInfo/ProgressLine.vue
@@ -16,6 +16,7 @@
           <el-progress
             color="#3464e0"
             :text-inside="true"
+            :show-text="false"
             :stroke-width="12"
             :percentage="(item?.ratio * 100).toFixed(2)"
           />
