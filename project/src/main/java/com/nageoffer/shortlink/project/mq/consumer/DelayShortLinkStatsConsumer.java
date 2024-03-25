dd311eaf0b33203d7b5791b9d06e32fbf099183c
chen.ma <machen@apache.org>
2024-03-25 22:45:09 +0800

    optimize: 删除延迟队列初始化逻辑

--- project/src/main/java/com/nageoffer/shortlink/project/mq/consumer/DelayShortLinkStatsConsumer.java
+++ project/src/main/java/com/nageoffer/shortlink/project/mq/consumer/DelayShortLinkStatsConsumer.java
@@ -87,6 +87,6 @@ public class DelayShortLinkStatsConsumer implements InitializingBean {
 
     @Override
     public void afterPropertiesSet() throws Exception {
-        onMessage();
+        // onMessage();
     }
 }
