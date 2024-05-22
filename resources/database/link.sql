1a2025adc4f1fe2cc3e17d8081f2abcff5a8f2fa
chen.ma <machen@apache.org>
2024-05-23 00:59:42 +0800

    fix: 修复分组唯一表索引错误

--- resources/database/link.sql
+++ resources/database/link.sql
@@ -227,7 +227,7 @@ CREATE TABLE `t_group_unique`
     `id`  bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',
     `gid` varchar(32) DEFAULT NULL COMMENT '分组标识',
     PRIMARY KEY (`id`),
-    KEY   `idx_username` (`username`) USING BTREE
+    UNIQUE KEY `idx_unique_gid` (`gid`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `t_link_0`
